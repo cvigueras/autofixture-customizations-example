e941f51963f8690e8a0a0660a683a0291020129b
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-05-25 16:55:51 +0200

    change: user class to inmutable object.

--- AutoFixture.Customizations.Example.App/User.cs
+++ AutoFixture.Customizations.Example.App/User.cs
@@ -2,17 +2,17 @@
 {
     public class User
     {
-        public string Name { get; set; }
-        public string LastName { get; set; }
-        public string City { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Gender { get; set; }
-        public string Email { get; set; }
-        public string MobileNumber { get; set; }
-        public int Age { get; set; }
+        public string Name { get; }
+        public string LastName { get; }
+        public string City { get; }
+        public string PhoneNumber { get; }
+        public string Gender { get; }
+        public string Email { get; }
+        public string MobileNumber { get; }
+        public int Age { get; }
 
 
-        public User(string name,
+        private User(string name,
                     string lastName,
                     string city,
                     string phoneNumber,
@@ -29,6 +29,19 @@
             Email = email;
             MobileNumber = mobileNumber;
             Age = age;
+        }
+
+        public static User Create(string name,
+                    string lastName,
+                    string city,
+                    string phoneNumber,
+                    string gender,
+                    string email,
+                    string mobileNumber,
+                    int age)
+        {
+            return new User(name, lastName, city, 
+                phoneNumber, gender, email, mobileNumber, age);
         }
     }
 }
