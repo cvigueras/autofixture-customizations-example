33a0ab9ed28dd836d1302acdbbcd2bfb7d7595c4
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-05-25 16:53:28 +0200

    feat: implemented home class.

---
+++ AutoFixture.Customizations.Example.App/Home.cs
@@ -0,0 +1,22 @@
+﻿namespace AutoFixture.Customizations.Example.App
+{
+    public class Home
+    {
+        public string Access(User user)
+        {
+            switch (user.Age)
+            {
+                case < 18:
+                    return "Aplicación web solo para personas mayores de edad";
+                case < 30:
+                    return "Consigue un descuento de 20€ en tu primer pedido";
+                case < 65:
+                    return "Consigue un descuento de 25€ en tu primer pedido";
+                case > 65:
+                    return "Consigue un descuento de 30€ en tu primer pedido";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
