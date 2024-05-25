f200bd8ec0fbf10e70107291177adedbcce3666c
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-05-25 17:10:42 +0200

    test: implemented get_user_successfully_when_create_with_autofixture

--- AutoFixture.Customizations.Example.App.Test/UserShould.cs
+++ AutoFixture.Customizations.Example.App.Test/UserShould.cs
@@ -1,10 +1,23 @@
+using AutoFixture;
+
 namespace AutoFixture.Customizations.Example.App.Test
 {
     public class UserShould
     {
+        private Fixture fixture;
+
         [SetUp]
         public void Setup()
         {
+            fixture = new Fixture();
+        }
+
+        [Test]
+        public void get_user_successfully_when_create_with_autofixture()
+        {
+            var user = fixture.Create<User>();
+
+            Assert.IsNotNull(user);
         }
     }
 }
